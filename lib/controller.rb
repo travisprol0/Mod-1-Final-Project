3fd9e4215d05d719fef0b34722868d170594ae9d
Andy Agus <Redagus@gmail.com>
2020-06-18 12:17:10 -0400

    updated colors

--- lib/controller.rb
+++ lib/controller.rb
@@ -19,8 +19,8 @@ class Controller
 		 prompt.select("What would you like to do?") do |menu|
 	      menu.choice "Create a sampler", -> {create_sampler}
 		  menu.choice "Use an existing sampler", -> {choose_sampler}
-		  menu.choice "Update a sampler", -> {update_sampler}
 		  menu.choice "Delete a sampler", -> {destroy_sampler}
+		  menu.choice "Update a sampler", -> {update_sampler}
 	  	end
 	end
 
@@ -39,18 +39,18 @@ class Controller
 				system "clear"
 		@new_board = (
 		puts "            "
-		puts "			    Your Sampler: #{@chosen_sampler.name}	"
+		puts "			    Your Sampler: #{@chosen_sampler.name}	".colorize(:yellow)
 		puts "            "		
-		puts "                                ==========="
+		puts "                                ===========".colorize(:yellow)
 		puts "				| #{@board[0]} | #{@board[1]} |"
-		puts "				―――――――――――"
+		puts "				―――――――――――".colorize(:yellow)
 		puts "				| #{@board[2]} | #{@board[3]} |")
-		puts "                                ==========="		
+		puts "                                ==========="	.colorize(:yellow)	
 		puts ""
 		puts "            "		
 
-		puts 'Select a number 1-4 to hear the sound'
-		puts 'Type "return" to return to main menu'
+		puts '		  Select a number 1-4 to hear the sound'.colorize(:yellow)	
+		puts '		  Type "return" to return to main menu'.colorize(:yellow)	
 		puts_sounds
 	end
 
@@ -60,14 +60,15 @@ class Controller
 				answer = answer.to_i
 				answer = answer - 1
 				if answer > 3 || answer < 0
-					puts 'Select a number 1-4 to hear the sound, or type "return" to return to main menu'
-					sleep(2)
+					puts '                    ** select a number from 1-4 **'.colorize(:red)
+					sleep(1)
 					system "clear"
 					display_board
 				else
 					@sounds = @chosen_sampler.sounds.map {|sound|sound.noise}
 					@sounds = @sounds[answer]
 					pid = fork{ exec 'afplay', "./db/audio/#{@sounds}.mp3" }
+					sleep(1)
 					system "clear"
 					display_board
 				end
@@ -76,7 +77,7 @@ class Controller
 				greetings
 			else
 				puts 'Select a number 1-4 to hear the sound, or type "return" to return to main menu'
-					sleep(2)
+					sleep(1)
 					system "clear"
 					display_board
 			end
@@ -121,62 +122,20 @@ class Controller
 	end
 
 	def update_sampler
-			@new_emojis = []
-			samplers = Sampler.all.map {|sampler| {sampler.name => sampler.id}}
-			sampler_id = prompt.select("Select a sampler to update", samplers)
-			@chosen_sampler = Sampler.find_by(id: sampler_id)
-
-			
-			
-			def update_name
-				@new_name = prompt.ask("Which do you want to rename your sampler?", required: true)
-				@chosen_sampler.update(name: @new_name)
-				
-				# binding.pry
-				puts "Your sampler has been updated"
-				sleep(2)
-				greetings
-			end
-			
-			def update_emojis
-				hi = SamplerSound.all.map {|sound| sound.sampler_id == @sampler_object}
-				
-				# 1. Select all SamplerSounds for this sampler
-				# 2. Iterate through all samplerSounds				for loop?
-				# 3. Change sounds
-				
-				
-				puts "Feature coming soon!"
-				sleep(2)
-				system "clear"
-				greetings
-				# binding.pry
-				# sleep(2)
-				# greetings
-
-
-				# puts "Select your first emoji!"
-				# 	emoji = Sound.all.map {|sounds| {sounds.emoji => sounds}}
-				# 	binding.pry
-				# 	@sound_arr = prompt.multi_select("Select 4 emojis Using Your Space 🚀 bar", emoji, min:4, max:4)
-				# 	SamplerSound.create(sound_id: @sound_arr[0].id, sampler_id: @new_sampler.id )
-				# 	@i = 0 
-				# 	while @i < 4
-				# 		SamplerSound.create(sound_id: @sound_arr[@i].id, sampler_id: @new_sampler.id )
-				# 		@i += 1
-				# 	end
-				# 	@new_emojis << @sound_arr
-			end
-			
-			prompt.select("What would you like to do?") do |menu|
-				menu.choice "Update name", -> {update_name}
-				menu.choice "Update emojis", -> {update_emojis}
-			end
-		end
+		puts "Here are your samplers: #{Sampler.all.map{|sampler| sampler.name}}"
+		old_name = prompt.ask("Which do you want to update?", required: true)
+		sampler_object = Sampler.all.find_by(name: old_name)
+		new_name = prompt.ask("Which do you want to rename your sampler?", required: true)
+		sampler_object.update(name: new_name)
+		puts "Your sampler has been updated"
+		sleep(1)
+		greetings
+	end
+	
 	def destroy_sampler
 		if Sampler.count == 0
 			puts "You have no samplers!"
-			sleep(2)
+			sleep(1)
 			greetings
 		else
 			puts "Here are your samplers:
@@ -184,7 +143,7 @@ class Controller
 			sampler_to_destroy = prompt.ask("Type the name of the sampler you would like to delete", required: true)
 			Sampler.find_by(name: sampler_to_destroy).destroy
 			puts "#{sampler_to_destroy} has been deleted!"
-			sleep(2)
+			sleep(1)
 			greetings
 		end
 	end
